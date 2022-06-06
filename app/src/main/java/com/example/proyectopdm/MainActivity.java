3963f5b03c206a2016eaf24b5916ed0da983794c
Daniel Romero Duarte <danred001@gmail.com>
2022-06-05 21:44:36 -0600

    adds changes

--- app/src/main/java/com/example/proyectopdm/MainActivity.java
+++ app/src/main/java/com/example/proyectopdm/MainActivity.java
@@ -1,33 +1,100 @@
 package com.example.proyectopdm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.Navigation;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.proyectopdm.bd.BD;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.lang.ref.Reference;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Variables para HttpurlConnection
+    EditText editText;
+    Button btn;
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
+
     //Declaracion de variables
     EditText user,pass;
     Button btnEntrar,btnRegistar;
     BD db;
     DT dt;
+
+    //Variable para libreria de audio
+    Button addVoice;
+
+    //Variables para libreria de Imagenes
+    ImageView imgGallery;
+    Button btnGallery;
+    private  final int GALLERY_REQ_CODE = 1000;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Conectar a Firebase
+        editText=findViewById(R.id.editFile);
+        btn=findViewById(R.id.btnFile);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference("proyectoPDM");
+
+        btn.setEnabled(false);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectPDF();
+            }
+        });
+
+
+
         //Enlazar las variables con el contenido del layout
 
         user=(EditText) findViewById(R.id.user);
         pass=(EditText) findViewById(R.id.password);
         btnEntrar=(Button) findViewById(R.id.btnIngresar);
+
+        //Codigo para libreria de imagenes
+        imgGallery=(ImageView) findViewById(R.id.imgPerfil);
+        btnGallery=(Button) findViewById(R.id.btnGallery);
+        btnGallery.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent iGallery = new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery, GALLERY_REQ_CODE);
+            }
+        });
+
+
+
+
        // btnRegistar=(Button) findViewById(R.id.btnRegistrarse);
         db=new BD(this);
 
@@ -38,6 +105,94 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         db.abrir();
         db.llenarDB();
         db.cerrar();
+    }
+
+    //Connection for Httpurlconnection
+    private void selectPDF(){
+        Intent intent=new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "PDF FILE SELECT"), 12);
+    }
+
+    //Codigo para libreria de audio
+    public void getSpeechInput(View view) {
+
+		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+		if (intent.resolveActivity(getPackageManager()) != null) {
+			startActivityForResult(intent, 10);
+		} else {
+			Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+    //Codigo para libreria de audio
+    @Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		//for connection
+        if(requestCode == 12 && resultCode == RESULT_OK && data != null && data.getData()!=null){
+            btn.setEnabled(true);
+            editText.setText(data.getDataString().substring(data.getDataString().lastIndexOf("/")+1));
+
+            btn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view){
+                    uploadPDFFileFirebase(data.getData());
+                }
+            });
+        }
+
+		//for image
+        if(resultCode == RESULT_OK){
+            if(requestCode == GALLERY_REQ_CODE){
+                imgGallery.setImageURI(data.getData());
+            }
+        }
+
+        //for voice
+		switch (requestCode) {
+			case 10:
+				if (resultCode == RESULT_OK && data != null) {
+					ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+					user.setText(result.get(0));
+				}
+				break;
+		}
+	}
+
+	//codigo para htpurlconnection
+    private void uploadPDFFileFirebase(Uri data){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("File is loading...");
+        progressDialog.show();
+        StorageReference reference = storageReference.child("uploadPDF"+System.currentTimeMillis()+".pdf");
+
+        reference.putFile(data).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>(){
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot){
+
+                Task<Uri> uriTask=taskSnapshot.getStorage().getDownloadUrl();
+                while(!uriTask.isComplete());
+                Uri uri=uriTask.getResult();
+                PutPDF putPDF = new PutPDF(editText.getText().toString(),uri.toString());
+                databaseReference.child(databaseReference.push().getKey()).setValue(putPDF);
+                Toast.makeText(MainActivity.this, "File Upload", Toast.LENGTH_LONG).show();
+                progressDialog.dismiss();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>(){
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot){
+
+                double progress = (100.0 * snapshot.getBytesTransferred())/snapshot.getTotalByteCount();
+                progressDialog.setMessage("File uploaded..."+(int) progress + "%");
+
+            }
+        });
     }
 
 
